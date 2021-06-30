94f30446bdfb84e2e26d249ff49cf6ba0e2ad7f1
Timo Anttila <14306628+timoanttila@users.noreply.github.com>
2021-06-30 21:05:19 +0300

    Create googleanalytics.svelte

---
+++ routify/googleanalytics.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { page } from '@roxi/routify';
+	$: {
+		if (typeof gtag !== "undefined"){
+			gtag("config", "UA-SOMEANALYTICSID-1", {
+				page_path: $page.path
+			});
+		}
+	}
+</script>
